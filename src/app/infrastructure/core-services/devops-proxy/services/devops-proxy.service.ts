1ac4c2f2b701b004adf5c69221f41d4519aba795
Matthias Müller <MatthiasM@Live.de>
2019-05-28 09:01:39 +0200

    WIP

--- src/app/infrastructure/core-services/devops-proxy/services/devops-proxy.service.ts
+++ src/app/infrastructure/core-services/devops-proxy/services/devops-proxy.service.ts
@@ -1,12 +1,10 @@
+/// <reference types="vss-web-extension-sdk" />
 import { Injectable } from '@angular/core';
-
-declare var VSS: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevopsProxyService {
-  // For API reference, see https://docs.microsoft.com/de-de/azure/devops/extend/reference/client/core-sdk?view=azure-devops
   public getCurrentUserName(): string {
     return VSS.getWebContext().user.name;
   }
